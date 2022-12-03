e77d14b365991f6341bb447c60b685c5dd42e55f
Alex Petrov <alex.petrov.vt@gmail.com>
2022-12-03 13:32:17 -0500

    finish day 2

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
 pub mod day1 {
-    pub fn part1(lines: &String) -> i32 {
+    pub fn part1(lines: std::str::Lines) -> i32 {
         use std::cmp;
 
         let mut max = 0;
         let mut current = 0;
 
-        for line in lines.lines() {
+        for line in lines {
             if line == "" {
                 max = cmp::max(max, current);
                 current = 0;
@@ -18,14 +18,14 @@ pub mod day1 {
         max
     }
 
-    pub fn part2(lines: &String) -> i32 {
+    pub fn part2(lines: std::str::Lines) -> i32 {
         let mut top1 = 0;
         let mut top2 = 0;
         let mut top3 = 0;
 
         let mut current = 0;
 
-        for line in lines.lines() {
+        for line in lines {
             if line == "" {
                 (top1, top2, top3) = update_top_scores(current, top1, top2, top3);
                 current = 0;
@@ -58,6 +58,133 @@ pub mod day1 {
     }
 }
 
+pub mod day2 {
+    #[derive(PartialEq, Eq)]
+    enum Shape {
+        Rock,
+        Paper,
+        Scissors,
+    }
+
+    impl Shape {
+        fn shape_to_beat(&self) -> Shape {
+            match self {
+                Shape::Rock => Shape::Scissors,
+                Shape::Paper => Shape::Rock,
+                Shape::Scissors => Shape::Paper,
+            }
+        }
+
+        fn shape_to_lose(&self) -> Shape {
+            match self {
+                Shape::Rock => Shape::Paper,
+                Shape::Paper => Shape::Scissors,
+                Shape::Scissors => Shape::Rock,
+            }
+        }
+    }
+
+    #[derive(PartialEq, Eq)]
+    enum RoundOutcome {
+        Loss,
+        Draw,
+        Win,
+    }
+
+    pub fn part1(lines: std::str::Lines) -> i32 {
+        let mut score = 0;
+
+        for line in lines {
+            let mut split = line.split(' ');
+            let opponent = split.next().unwrap();
+            let me = split.next().unwrap();
+
+            let outcome = play_round(char_to_shape(me), char_to_shape(opponent));
+
+            score += shape_to_score(char_to_shape(me)) + outcome_to_score(outcome)
+        }
+
+        score
+    }
+
+    pub fn part2(lines: std::str::Lines) -> i32 {
+        let mut score = 0;
+
+        for line in lines {
+            let mut split = line.split(' ');
+            let opponent = split.next().unwrap();
+            let target_outcome = split.next().unwrap();
+
+            let shape_to_choose =
+                determine_shape(char_to_outcome(target_outcome), char_to_shape(opponent));
+
+            score +=
+                shape_to_score(shape_to_choose) + outcome_to_score(char_to_outcome(target_outcome))
+        }
+
+        score
+    }
+
+    fn char_to_shape(c: &str) -> Shape {
+        match c {
+            "A" => Shape::Rock,
+            "B" => Shape::Paper,
+            "C" => Shape::Scissors,
+            "X" => Shape::Rock,
+            "Y" => Shape::Paper,
+            "Z" => Shape::Scissors,
+
+            // shouldn't happen
+            _ => Shape::Rock,
+        }
+    }
+
+    fn play_round(me: Shape, opponent: Shape) -> RoundOutcome {
+        if me == opponent {
+            return RoundOutcome::Draw;
+        } else if me.shape_to_beat() == opponent {
+            return RoundOutcome::Win;
+        } else {
+            return RoundOutcome::Loss;
+        }
+    }
+
+    fn determine_shape(target_outcome: RoundOutcome, opponent: Shape) -> Shape {
+        match target_outcome {
+            RoundOutcome::Win => opponent.shape_to_lose(),
+            RoundOutcome::Draw => opponent,
+            RoundOutcome::Loss => opponent.shape_to_beat(),
+        }
+    }
+
+    fn shape_to_score(s: Shape) -> i32 {
+        match s {
+            Shape::Rock => 1,
+            Shape::Paper => 2,
+            Shape::Scissors => 3,
+        }
+    }
+
+    fn outcome_to_score(r: RoundOutcome) -> i32 {
+        match r {
+            RoundOutcome::Loss => 0,
+            RoundOutcome::Draw => 3,
+            RoundOutcome::Win => 6,
+        }
+    }
+
+    fn char_to_outcome(c: &str) -> RoundOutcome {
+        match c {
+            "X" => RoundOutcome::Loss,
+            "Y" => RoundOutcome::Draw,
+            "Z" => RoundOutcome::Win,
+
+            // shouldn't happen
+            _ => RoundOutcome::Loss,
+        }
+    }
+}
+
 mod tests {
     #[test]
     fn day1_part1() {
@@ -78,7 +205,7 @@ mod tests {
 10000
 ";
 
-        assert_eq!(crate::day1::part1(&String::from(input)), 24000)
+        assert_eq!(crate::day1::part1(input.lines()), 24000)
     }
 
     #[test]
@@ -100,6 +227,28 @@ mod tests {
 10000
 ";
 
-        assert_eq!(crate::day1::part2(&String::from(input)), 45000)
+        assert_eq!(crate::day1::part2(input.lines()), 45000)
+    }
+
+    #[test]
+    fn day2_part1() {
+        let input = "\
+A Y
+B X
+C Z
+";
+
+        assert_eq!(crate::day2::part1(input.lines()), 15)
+    }
+
+    #[test]
+    fn day2_part2() {
+        let input = "\
+A Y
+B X
+C Z
+";
+
+        assert_eq!(crate::day2::part2(input.lines()), 12)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
-use advent_of_code_2022_rust::day1;
+use advent_of_code_2022_rust::{day1, day2};
 use std::fs;
 
 fn main() {
-    let lines = fs::read_to_string("input/day1.txt").unwrap();
+    println!("=== DAY 1 ===");
+    let contents = fs::read_to_string("input/day1.txt").unwrap();
+    println!("{}", day1::part1(contents.lines()));
+    println!("{}", day1::part2(contents.lines()));
 
-    println!("{}", day1::part1(&lines));
-    println!("{}", day1::part2(&lines));
+    println!("=== DAY 2 ===");
+    let contents = fs::read_to_string("input/day2.txt").unwrap();
+    println!("{}", day2::part1(contents.lines()));
+    println!("{}", day2::part2(contents.lines()));
 }
